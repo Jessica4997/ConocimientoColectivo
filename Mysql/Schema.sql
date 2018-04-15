10dc66b00842e4d43e21337c3566b17a2834a841
Jessica Paredes <jessp.4997@gmail.com>
2018-04-15 05:56:52 -0500

    fix(mysql): nuevos nombres a los FK

--- Mysql/Schema.sql
+++ Mysql/Schema.sql
@@ -1,219 +1,185 @@
-CREATE DATABASE bc_cc2;
-CREATE TABLE IF NOT EXISTS `bd_cc2`.`users` (
-  `id` INT NOT NULL COMMENT '',
-  `name` VARCHAR(20) NOT NULL COMMENT '',
-  `last_name` VARCHAR(20) NOT NULL COMMENT '',
-  `email` VARCHAR(30) NOT NULL COMMENT '',
-  `password` VARCHAR(20) NOT NULL COMMENT '',
-  `cell_phone` VARCHAR(9) NOT NULL COMMENT '',
-  `phone` VARCHAR(7) NULL COMMENT '',
-  `date_birth` DATE NULL COMMENT '',
-  `description` VARCHAR(100) NULL COMMENT '',
-  `created_date` DATETIME NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '')
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.1.31-MariaDB : Database - db_cc_v2
+*********************************************************************
+*/
 
 
--- -----------------------------------------------------
--- Table `bd_cc`.`categories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bd_cc2`.`categories` (
-  `id` INT NOT NULL COMMENT '',
-  `cultural_theme` VARCHAR(20) NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '')
-ENGINE = InnoDB;
+/*!40101 SET NAMES utf8 */;
 
+/*!40101 SET SQL_MODE=''*/;
 
--- -----------------------------------------------------
--- Table `bd_cc`.`workshops`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bd_cc`.`workshops` (
-  `id` INT NOT NULL COMMENT '',
-  `amount` DECIMAL(4,2) NOT NULL COMMENT '',
-  `start_date` DATETIME NOT NULL COMMENT '',
-  `final_date` DATETIME NOT NULL COMMENT '',
-  `level` ENUM('Basico', 'Intermedio', 'Avanzado') NOT NULL COMMENT '',
-  `wrks_status` ENUM('En curso', 'Cancelado', 'Finalizado') NOT NULL COMMENT '',
-  `created_date` DATETIME NOT NULL COMMENT '',
-  `user_id` INT NULL COMMENT '',
-  `category_id` INT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  INDEX `fk_user_id_idx` (`user_id` ASC)  COMMENT '',
-  INDEX `fk_category_id_idx` (`category_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `bd_cc`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_category_id`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `bd_cc`.`categories` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_cc_v2` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci */;
 
+USE `db_cc_v2`;
 
--- -----------------------------------------------------
--- Table `bd_cc`.`inscribed_users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bd_cc2`.`inscribed_users` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `created_date` DATETIME NOT NULL COMMENT '',
-  `iu_status` ENUM('Confirmado','No confirmado') NOT NULL COMMENT '',
-  `student_rating` FLOAT NULL COMMENT '',
-  `tutor_rating` FLOAT NULL COMMENT '',
-  `user_id` INT NULL COMMENT '',
-  `wrks_id` INT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  INDEX `fk_user_id_idx` (`user_id` ASC)  COMMENT '',
-  INDEX `fk_wrks_id_idx` (`wrks_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `bd_cc`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_wrks_id`
-    FOREIGN KEY (`wrks_id`)
-    REFERENCES `bd_cc`.`workshops` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+/*Table structure for table `categories` */
 
+DROP TABLE IF EXISTS `categories`;
 
--- -----------------------------------------------------
--- Table `bd_cc`.`proposed_workshops`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bd_cc2`.`proposed_workshops` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `created_date` DATETIME NOT NULL COMMENT '',
-  `pw_status` ENUM('Activo','Inactivo') NOT NULL COMMENT '',
-  `user_id` INT NULL COMMENT '',
-  `category_id` INT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  INDEX `fk_user_id_idx` (`user_id` ASC)  COMMENT '',
-  INDEX `fk_category_id_idx` (`category_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `bd_cc`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_category_id`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `bd_cc`.`categories` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `cultural_theme` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+/*Table structure for table `cultural_theme` */
 
--- -----------------------------------------------------
--- Table `bd_cc`.`ratings`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bd_cc2`.`ratings` (
-  `user_id` INT NOT NULL COMMENT '',
-  `role` ENUM('Alumno', 'Docente') NOT NULL COMMENT '',
-  `final_rating` FLOAT NULL COMMENT '',
-  PRIMARY KEY (`user_id`, `role`)  COMMENT '',
-  CONSTRAINT `fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `bd_cc`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `cultural_theme`;
 
+CREATE TABLE `cultural_theme` (
+  `id` int(11) NOT NULL,
+  `description` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cultural_theme_category_id` (`category_id`),
+  CONSTRAINT `fk_cultural_theme_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- -----------------------------------------------------
--- Table `bd_cc`.`proposal_interested`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bd_cc2`.`proposal_interested` (
-  `id` INT NOT NULL COMMENT '',
-  `created_date` DATETIME NOT NULL COMMENT '',
-  `user_id` INT NULL COMMENT '',
-  `pw_id` INT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  INDEX `fk_user_id_idx` (`user_id` ASC)  COMMENT '',
-  INDEX `fk_pw_id_idx` (`pw_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `bd_cc`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pw_id`
-    FOREIGN KEY (`pw_id`)
-    REFERENCES `bd_cc`.`proposed_workshops` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+/*Table structure for table `discounts` */
 
+DROP TABLE IF EXISTS `discounts`;
 
--- -----------------------------------------------------
--- Table `bd_cc`.`discounts`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bd_cc2`.`discounts` (
-  `id` INT NOT NULL COMMENT '',
-  `final_rating` INT NULL COMMENT '',
-  `percentage` FLOAT NULL COMMENT '',
-  `discount` FLOAT NULL COMMENT '',
-  `wrks_id` INT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  INDEX `fk_wrks_id_idx` (`wrks_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_wrks_id`
-    FOREIGN KEY (`wrks_id`)
-    REFERENCES `bd_cc`.`workshops` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE `discounts` (
+  `id` int(11) NOT NULL,
+  `final_rating` int(11) DEFAULT NULL,
+  `percentage` float DEFAULT NULL,
+  `discount` float DEFAULT NULL,
+  `wrks_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_discounts_wrks_id` (`wrks_id`),
+  CONSTRAINT `fk_discounts_wrks_id` FOREIGN KEY (`wrks_id`) REFERENCES `workshops` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+/*Table structure for table `inscribed_users` */
 
--- -----------------------------------------------------
--- Table `bd_cc`.`payments`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bd_cc2`.`payments` (
-  `id` INT NOT NULL COMMENT '',
-  `amount` DECIMAL(4,2) NOT NULL COMMENT '',
-  `pay_status` ENUM('Pagado', 'No pagado') NOT NULL COMMENT '',
-  `created_date` DATETIME NOT NULL COMMENT '',
-  `dsct_id` INT NULL COMMENT '',
-  `user_id` INT NOT NULL COMMENT '',
-  `wrks_id` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  INDEX `fk_dsct_id_idx` (`dsct_id` ASC)  COMMENT '',
-  INDEX `fk_user_id_idx` (`user_id` ASC)  COMMENT '',
-  INDEX `fk_wrks_id_idx` (`wrks_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_dsct_id`
-    FOREIGN KEY (`dsct_id`)
-    REFERENCES `bd_cc`.`discounts` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `bd_cc`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_wrks_id`
-    FOREIGN KEY (`wrks_id`)
-    REFERENCES `bd_cc`.`workshops` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `inscribed_users`;
 
-USE `bd_cc2` ;
+CREATE TABLE `inscribed_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_date` datetime NOT NULL,
+  `iu_status` enum('Confirmado','No confirmado') NOT NULL,
+  `student_rating` float DEFAULT NULL,
+  `tutor_rating` float DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `wrks_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_inscribed_users_user_id` (`user_id`),
+  KEY `fk_inscribed_users_wrks_id` (`wrks_id`),
+  CONSTRAINT `fk_inscribed_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_inscribed_users_wrks_id` FOREIGN KEY (`wrks_id`) REFERENCES `workshops` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- -----------------------------------------------------
--- Table `BD_CC1`.`cultural_theme`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bd_cc2`.`cultural_theme` (
-  `id` INT NOT NULL COMMENT '',
-  `description` VARCHAR(45) NUusuarioLL COMMENT '',
-  `category_id` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  INDEX `fk_category_id_idx` (`category_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_category_id`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `bd_cc`.`categories` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+/*Table structure for table `payments` */
+
+DROP TABLE IF EXISTS `payments`;
+
+CREATE TABLE `payments` (
+  `id` int(11) NOT NULL,
+  `amount` decimal(4,2) NOT NULL,
+  `pay_status` enum('Pagado','No pagado') COLLATE utf8_spanish2_ci NOT NULL,
+  `created_date` datetime NOT NULL,
+  `dsct_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `wrks_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_payments_dsct_id` (`dsct_id`),
+  KEY `fk_payments_user_id` (`user_id`),
+  KEY `fk_payments_wrks_id` (`wrks_id`),
+  CONSTRAINT `fk_payments_dsct_id` FOREIGN KEY (`dsct_id`) REFERENCES `discounts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_payments_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_payments_wrks_id` FOREIGN KEY (`wrks_id`) REFERENCES `workshops` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+/*Table structure for table `proposal_interested` */
+
+DROP TABLE IF EXISTS `proposal_interested`;
+
+CREATE TABLE `proposal_interested` (
+  `id` int(11) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `pw_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_proposal_interested_user_id` (`user_id`),
+  KEY `fk_proposal_interested_pw_id` (`pw_id`),
+  CONSTRAINT `fk_proposal_interested_pw_id` FOREIGN KEY (`pw_id`) REFERENCES `proposed_workshops` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_proposal_interested_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+/*Table structure for table `proposed_workshops` */
+
+DROP TABLE IF EXISTS `proposed_workshops`;
+
+CREATE TABLE `proposed_workshops` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_date` datetime NOT NULL,
+  `pw_status` enum('Activo','Inactivo') NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_proposed_workshops_user_id` (`user_id`),
+  KEY `fk_proposed_workshops_category_id` (`category_id`),
+  CONSTRAINT `fk_proposed_workshops_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_proposed_workshops_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `ratings` */
+
+DROP TABLE IF EXISTS `ratings`;
+
+CREATE TABLE `ratings` (
+  `user_id` int(11) NOT NULL,
+  `role` enum('Alumno','Docente') COLLATE utf8_spanish2_ci NOT NULL,
+  `final_rating` float DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`role`),
+  CONSTRAINT `fk_ratings_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `cell_phone` varchar(9) NOT NULL,
+  `phone` varchar(7) DEFAULT NULL,
+  `date_birth` date DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `created_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `workshops` */
+
+DROP TABLE IF EXISTS `workshops`;
+
+CREATE TABLE `workshops` (
+  `id` int(11) NOT NULL,
+  `amount` decimal(4,2) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `final_date` datetime NOT NULL,
+  `level` enum('Basico','Intermedio','Avanzado') NOT NULL,
+  `wrks_status` enum('En curso','Cancelado','Finalizado') NOT NULL,
+  `created_date` datetime NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_workshops_user_id` (`user_id`),
+  KEY `fk_workshops_category_id` (`category_id`),
+  CONSTRAINT `fk_workshops_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_workshops_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
