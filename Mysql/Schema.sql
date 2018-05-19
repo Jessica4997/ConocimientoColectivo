f072c1acf084bfa6bcbc41d89cf3aa1c7a63ed67
Jessica <jessp.4997@gmail.com>
2018-05-19 00:46:15 -0500

    exportando la bd

--- Mysql/Schema.sql
+++ Mysql/Schema.sql
@@ -125,7 +125,7 @@ CREATE TABLE `proposed_workshops` (
   `final_date` datetime DEFAULT NULL,
   `level` enum('Básico','Intermedio','Avanzado') DEFAULT NULL,
   `pw_status` enum('Activo','Inactivo','Borrado') NOT NULL,
-  `created_date` datetime NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `user_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `subcategory_id` int(11) DEFAULT NULL,
@@ -142,7 +142,7 @@ CREATE TABLE `proposed_workshops` (
 
 /*Data for the table `proposed_workshops` */
 
-insert  into `proposed_workshops`(`id`,`title`,`description`,`start_date`,`final_date`,`level`,`pw_status`,`created_date`,`user_id`,`category_id`,`subcategory_id`,`removed`,`votes_quantity`) values (1,'Taller de Hip-Hop','Somos 5 chicos que deseamos la apertura de un taller de música urbana para el miércoles 25 de abril.','2018-04-25 14:00:00','2018-04-25 17:00:00','Básico','Inactivo','2018-04-25 08:03:19',2,1,23,'Activo',1),(2,'Taller de Ukelele','Buscamos a una persona que nos dicte clases de ukelele','2018-04-26 12:00:00','2018-04-26 14:00:00','Intermedio','Inactivo','2018-04-25 12:25:01',1,3,24,'Eliminado',0),(3,'Taller de Pintura','Necesitamos un profesor que nos enseñe como pintar en degradado','2018-04-25 18:00:00','2018-04-25 20:00:00','Avanzado','Inactivo','2018-04-25 12:26:34',3,5,16,'Activo',0),(6,'Taller de Ballet','Buscamos a una profesora que nos enseñe clases de ballet para una presentación','2018-04-25 08:00:00','2018-04-25 09:30:00','Intermedio','Inactivo','0000-00-00 00:00:00',2,1,22,'Activo',0),(7,'Taller de Fulbito','','2018-06-05 16:00:00','2018-06-05 18:00:00','Avanzado','Inactivo','0000-00-00 00:00:00',2,2,5,'Activo',0);
+insert  into `proposed_workshops`(`id`,`title`,`description`,`start_date`,`final_date`,`level`,`pw_status`,`created_date`,`user_id`,`category_id`,`subcategory_id`,`removed`,`votes_quantity`) values (1,'Taller de Hip-Hop','Somos 5 chicos que deseamos la apertura de un taller de música urbana para el miércoles 25 de abril.','2018-04-25 14:00:00','2018-04-25 17:00:00','Básico','Inactivo','2018-04-25 08:03:19',2,1,23,'Activo',1),(2,'Taller de Ukelele','Buscamos a una persona que nos dicte clases de ukelele','2018-04-26 12:00:00','2018-04-26 14:00:00','Intermedio','Activo','2018-04-25 12:25:01',1,3,24,'Eliminado',0),(3,'Taller de Pintura','Necesitamos un profesor que nos enseñe como pintar en degradado','2018-04-25 18:00:00','2018-04-25 20:00:00','Avanzado','Inactivo','2018-04-25 12:26:34',3,5,16,'Activo',0),(6,'Taller de Ballet','Buscamos a una profesora que nos enseñe clases de ballet para una presentación','2018-04-25 08:00:00','2018-04-25 09:30:00','Intermedio','Inactivo','0000-00-00 00:00:00',2,1,22,'Activo',0),(7,'Taller de Fulbito','d','2018-06-05 16:00:00','2018-06-05 18:00:00','Avanzado','Inactivo','0000-00-00 00:00:00',2,2,5,'Activo',0),(9,'Clases de Bachata','Taller de bachata para personas con conocimiento intermedios','2018-05-19 08:00:00','2018-05-19 10:00:00','Intermedio','Inactivo','0000-00-00 00:00:00',3,1,1,'Activo',0);
 
 /*Table structure for table `proposed_workshops_votes` */
 
@@ -210,15 +210,15 @@ CREATE TABLE `users` (
   `phone` varchar(7) DEFAULT NULL,
   `date_birth` date DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
-  `created_date` datetime NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `gender` enum('Femenino','Masculino') NOT NULL,
   `removed` enum('Activo','Eliminado') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`name`,`last_name`,`email`,`password`,`cell_phone`,`phone`,`date_birth`,`description`,`created_date`,`gender`,`removed`) values (1,'Jessica Edith','Paredes Alarcon','jessp.4997@gmail.com','123456','958690578','5341313','1997-09-04','Hola','2018-04-15 06:17:38','Femenino','Activo'),(2,'Kevin','Robles','kevin0696@gmail.com','56987','963852741','456123','1996-11-06','fdgdfgdg','0000-00-00 00:00:00','Masculino','Activo'),(3,'Ana','Suarez','ana@gmail.com','123','963852741','7894256','0000-00-00','sadsada','0000-00-00 00:00:00','Femenino','Activo'),(12,'prueba2','prueba2','asa@aa','prueba2','','','0000-00-00','','0000-00-00 00:00:00','Masculino','Eliminado');
+insert  into `users`(`id`,`name`,`last_name`,`email`,`password`,`cell_phone`,`phone`,`date_birth`,`description`,`created_date`,`gender`,`removed`) values (1,'Jessica Edith','Paredes Alarcon','jessp.4997@gmail.com','123456','958690578','5341313','1997-09-04','Hola','2018-04-15 06:17:38','Femenino','Activo'),(2,'Kevin','Robles','kevin0696@gmail.com','56987','963852741','456123','1996-11-06','fdgdfgdg','0000-00-00 00:00:00','Masculino','Activo'),(3,'Ana','Suarez','ana@gmail.com','123','963852741','7894256','0000-00-00','sadsada','0000-00-00 00:00:00','Femenino','Activo'),(12,'prueba2','prueba2','asa@aa','prueba2','','','0000-00-00','','0000-00-00 00:00:00','Masculino','Eliminado'),(18,'a','a','p@a','a','','','0000-00-00','','2018-05-18 22:43:23','Femenino','Activo');
 
 /*Table structure for table `workshops` */
 
@@ -234,10 +234,11 @@ CREATE TABLE `workshops` (
   `final_date` datetime NOT NULL,
   `level` enum('Básico','Intermedio','Avanzado') NOT NULL,
   `wrks_status` enum('En curso','Cancelado','Finalizado') DEFAULT NULL,
-  `created_date` datetime NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `user_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `subcategory_id` int(11) DEFAULT NULL,
+  `removed` enum('Activo','Eliminado') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_workshops_user_id` (`user_id`),
   KEY `fk_workshops_category_id` (`category_id`),
@@ -245,11 +246,11 @@ CREATE TABLE `workshops` (
   CONSTRAINT `fk_workshops_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_workshops_subcategory_id_subcategories_id` FOREIGN KEY (`subcategory_id`) REFERENCES `subcategories` (`id`),
   CONSTRAINT `fk_workshops_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8;
 
 /*Data for the table `workshops` */
 
-insert  into `workshops`(`id`,`title`,`description`,`vacancy`,`amount`,`start_date`,`final_date`,`level`,`wrks_status`,`created_date`,`user_id`,`category_id`,`subcategory_id`) values (1,'Clase de Guitarra','Buscamos 5 personas que quieran aprender a tocar guitarra.',5,100.00,'2018-04-22 06:20:37','2018-04-22 06:20:27','Básico','En curso','2018-04-15 06:20:16',1,3,10),(2,'Clase de Baile','Busco 10 personas que esten dispuestas a aprender clases de salsa en nivel intermedio.',10,50.00,'2018-04-21 06:39:35','2018-04-25 06:39:40','Intermedio','En curso','2018-04-15 06:39:52',2,1,2),(3,'Clase de Repostería','Personas interesadas a aprender como preparar postres postular al taller.',8,150.00,'2018-04-25 12:49:25','2018-04-28 12:49:33','Avanzado','En curso','2018-04-15 12:49:54',1,6,19),(4,'Clase de Batería','',3,40.00,'2018-04-22 12:51:23','2018-04-23 12:51:58','Intermedio','En curso','2018-04-15 12:52:18',2,3,11),(50,'Taller de Violín','Buscamos jóvenes que quieran aprender a tocar violín con un profesor con 10 años de trayectoria',10,30.00,'2018-04-25 17:00:00','2018-04-25 18:30:00','Intermedio','En curso','0000-00-00 00:00:00',2,3,28),(76,'Taller de Ukelele','2',2,2.00,'0000-00-00 00:00:00','0000-00-00 00:00:00','Intermedio','En curso','0000-00-00 00:00:00',2,3,24);
+insert  into `workshops`(`id`,`title`,`description`,`vacancy`,`amount`,`start_date`,`final_date`,`level`,`wrks_status`,`created_date`,`user_id`,`category_id`,`subcategory_id`,`removed`) values (1,'Clase de Guitarra','Buscamos 5 personas que quieran aprender a tocar guitarra.',5,100.00,'2018-04-22 06:20:37','2018-04-22 06:20:27','Básico','En curso','2018-04-15 06:20:16',1,3,10,'Activo'),(2,'Clase de Baile','Busco 10 personas que esten dispuestas a aprender clases de salsa en nivel intermedio.',10,50.00,'2018-04-21 06:39:35','2018-04-25 06:39:40','Intermedio','En curso','2018-04-15 06:39:52',2,1,2,'Activo'),(3,'Clase de Repostería','Personas interesadas a aprender como preparar postres postular al taller.',8,150.00,'2018-04-25 12:49:25','2018-04-28 12:49:33','Avanzado','En curso','2018-04-15 12:49:54',1,6,19,'Activo'),(4,'Clase de Batería','',3,40.00,'2018-04-22 12:51:23','2018-04-23 12:51:58','Intermedio','En curso','2018-05-19 00:13:16',2,3,11,'Activo'),(50,'Taller de Violín','Buscamos jóvenes que quieran aprender a tocar violín con un profesor con 10 años de trayectoria',10,30.00,'2018-04-25 17:00:00','2018-04-25 18:30:00','Intermedio','En curso','0000-00-00 00:00:00',2,3,28,'Activo'),(76,'Taller de Ukelele','2',2,2.00,'0000-00-00 00:00:00','0000-00-00 00:00:00','Intermedio','En curso','2018-05-19 00:35:40',2,3,24,'Activo'),(80,'Taller de Comida Oriental','Comida oriental',5,32.00,'2018-05-18 18:30:00','2018-05-18 20:00:00','Intermedio','En curso','0000-00-00 00:00:00',1,6,20,'Activo');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
